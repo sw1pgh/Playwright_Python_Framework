b1fec78090298c76e0f019a6ab2a225023671984
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2025-08-23 10:28:20 +0530

    Code Updated

--- Page_Objects/google_homepage_locators.py
+++ Page_Objects/google_homepage_locators.py
@@ -5,4 +5,4 @@ class Google_HomePage_Locators:
         self.page = page
         self.google_image = page.locator("//*[name()='path' and contains(@d,'M115.75 47')]")
         self.feeling_lucky_btn = self.page.locator("div[class='FPdoLc lJ9FBc'] input[name='btnI']")
-        self.google_doole_glue_header = self.page.locator("div[id='glue-drawer-2465973'] a[title='Google doodles']")
+        self.google_doodle_glue_header = self.page.locator("div[id='glue-drawer-2465973'] a[title='Google doodles']")
